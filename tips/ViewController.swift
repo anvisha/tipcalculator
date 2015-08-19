c0fb1986ca122f2ae3f4b0ff13a1cf7944ce039d
anvisha <anvisha@gmail.com>
2015-08-18 23:00:13 -0700

    updated README

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //UI Labels
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    
+    @IBOutlet weak var billFieldContainer: UIView!
+    @IBOutlet weak var totalLabelContainer: UIView!
+    //Global Vars
     var tipRightNow = 0.0
     var minTip = 0.0
     var defaultTip = 0.0
@@ -22,11 +25,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setStartState()
         handleDefaults()
-        
         // Do any additional setup after loading the view, typically from a nib.
-        updateState()
         dismissViewControllerAnimated(true, completion: nil)
         billField.becomeFirstResponder()}
     
@@ -51,6 +52,26 @@ class ViewController: UIViewController {
         tipRightNow = defaultTip
 
     }
+    
+    func setStartState() {
+        self.totalLabelContainer.hidden = true
+        self.tipLabel.text = ""
+        self.billFieldContainer.backgroundColor = self.UIColorFromRGB(0xFFFFFF)
+        self.billFieldContainer.frame.origin.y = 176
+        self.totalLabelContainer.frame.origin.y = 387
+
+    }
+    
+    func setEditingState() {
+        self.billFieldContainer.backgroundColor = self.UIColorFromRGB(0x82968C)
+        self.billFieldContainer.frame.origin.y = 62
+        UIView.animateWithDuration(0.6, animations: {
+            self.totalLabelContainer.frame.origin.y = 254
+            self.totalLabelContainer.hidden = false
+//            self.totalLabelContainer.backgroundColor = self.UIColorFromRGB(0x4C3B4D)
+
+        })
+    }
 
 
     override func didReceiveMemoryWarning() {
@@ -69,8 +90,18 @@ class ViewController: UIViewController {
     
    
     @IBAction func onEditingChanged(sender: AnyObject) {
-        
-        updateState()
+        if billField.text == "" {
+            self.totalLabel.hidden = true
+            UIView.animateWithDuration(0.6, animations: {
+                self.setStartState()
+            })
+        }
+        if billField.text != "" {
+            UIView.animateWithDuration(0.4, animations: {
+                self.setEditingState()
+            })
+            updateState()
+        }
 
     }
     
@@ -96,5 +127,16 @@ class ViewController: UIViewController {
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
+    
+    //HELPERS
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+
 }
 
